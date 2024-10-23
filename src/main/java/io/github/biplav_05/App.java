b267d11a42fc40796d0064b01b9fa7127e20e71c
BIPLAV00 <biplav.ghimire@truenary.com>
2024-10-23 11:23:31 +0545

    feat: initail setup

---
+++ src/main/java/io/github/biplav_05/App.java
@@ -0,0 +1,13 @@
+package io.github.biplav_05;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
